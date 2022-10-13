e12b9b939be60c2c530c51858b65f6e50c753605
FOWMind <alwl1@hotmail.com>
2022-10-13 07:41:00 -0300

    [Refactor] Moved theme object to its own file

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,8 @@
 import "../styles/globals.css";
 import styled, { ThemeProvider } from "styled-components";
-import Head from "next/head";
 import GlobalStyle from "../styles/globalStyle";
-
-const theme = {
-  wrapper: {
-    bg: "linear-gradient(180deg, #1a1b20 0%, #0e0e11 100%)",
-  },
-  workItemOverlay: {
-    bg: "linear-gradient(180deg, transparent, #15161a)",
-  },
-  scroll: {
-    trackBg: "#292b33",
-    handlerBg: "#0e0e11",
-    handlerBgHover: "#3490E4",
-  },
-  primary: {
-    clr: "#fff",
-    bg: "#3490E4",
-    textShadow: "0 0 5px #fff",
-    boxShadow: "0px 0px 15px 0 #3490e4",
-    boxShadowBig: "0px 0px 35px 10px #3490e4",
-  },
-  secondary: {
-    bg: "#292b33",
-  },
-  featured: {
-    clr: "#3490E4",
-    bg: "linear-gradient(105deg,#006FD6 15%,#3490E4 55%,#75BDFF 100%)",
-    textShadow: "0 0 5px #3490E4",
-    boxShadow: "0px 0px 15px 0 #3490E4",
-  },
-};
+import Head from "next/head";
+import { theme } from "../theme";
 
 function MyApp({ Component, pageProps }) {
   return (

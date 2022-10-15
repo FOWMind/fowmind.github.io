cee3021331d871d04ee8a3959f9638b984cbb362
FOWMind <alwl1@hotmail.com>
2022-10-15 15:39:22 -0300

    [Fix] Bug with body overflow

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MenuView } from "./MenuView";
 
 export function Menu({ repositories }) {
   const [menuVisible, setMenuVisible] = useState(false);
 
+  useEffect(() => {
+    if (menuVisible) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "auto";
+    }
+  }, [menuVisible]);
+
   const toggleMenu = () => {
     setMenuVisible((x) => !x);
-    const currentBodyOverflowY = document.body.style.overflowY;
-    document.body.style.overflowY =
-      currentBodyOverflowY === "hidden" ? "auto" : "hidden";
   };
 
   return (
